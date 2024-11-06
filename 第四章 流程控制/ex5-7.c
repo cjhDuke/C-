ff64ab1921a25f782cd1d58e55cede59542f86f1
Duke Chen <132365209+cjhDuke@users.noreply.github.com>
2024-11-06 08:25:27 +0800

    Add files via upload

--- 第四章 流程控制/ex5-7.c
+++ 第四章 流程控制/ex5-7.c
@@ -4,10 +4,10 @@ int main()
 {
     float n,x=1,y;
     scanf("%f",&n);
+    x=sqrt(n);
     for(int i=1;i<n;i++)
     {
-        x=sqrt(n);
-        y=sqrt(n-i+x);
+        x=sqrt(n-i+x);
     }
-    printf("%f",y);
+    printf("%f",x);
 }
