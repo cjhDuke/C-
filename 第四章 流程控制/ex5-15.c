ebd7114696351f78074d4aa924fda5e1f39ee53f
Duke Chen <132365209+cjhDuke@users.noreply.github.com>
2024-11-06 16:37:25 +0800

    Add files via upload

--- 第四章 流程控制/ex5-15.c
+++ 第四章 流程控制/ex5-15.c
@@ -3,21 +3,22 @@
 int main()
 {
     float x,a=0,b=5,m,ya,yb,ym;
-    m=(a+b)/2;
     ya=a*a+a*sin(a)-5;
     yb=b*b+b*sin(b)-5;
-    ym=m*m+m*sin(m)-5;
-    while(fabs(ym)>1e-5)
+    do
     {
+        m=(a+b)/2;
         ym=m*m+m*sin(m)-5;
         if(ym*ya<0)
         {
-            m=(m+a)/2;
+            b=m;
+            yb=ym;
         }
         else
         {
-            m=(m+b)/2;
+            a=m;
+            ya=ym;
         }
-    }
+    }while(fabs(ym)>1e-5);
     printf("其中一个实根是:%f,此时方程的值是%f",m,ym);
 }
