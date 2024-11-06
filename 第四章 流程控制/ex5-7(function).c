c62dc4f964fdc3b233dd952e584a5d55bff98559
Duke Chen <132365209+cjhDuke@users.noreply.github.com>
2024-11-06 12:31:10 +0800

    Add files via upload

---
+++ 第四章 流程控制/ex5-7(function).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+float wozuishuai(int n,int depth);
+ int main()
+ {
+    int n;
+    scanf("%d",&n);
+    float y=wozuishuai(n,n);
+    printf("%f",y);
+ }
+ float wozuishuai(int n,int depth)
+ {
+    if(depth<=1)
+    {
+      return sqrt(n);
+    }
+    else
+    {
+      return sqrt(n-depth+1+wozuishuai(n-1,n-1));
+    }
+ }
+
+ 
