60a220a3bbe9c2e1446ddbbd3395c3a6be23e46d
Duke Chen <132365209+cjhDuke@users.noreply.github.com>
2024-11-14 21:45:34 +0800

    Add files via upload

---
+++ 第二章 基本数据类型与常用库函数/ex10-2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a;
+    float b;
+    double c;
+    scanf("%d%f%lf",&a,&b,&c);
+    printf("%d %f %lf",a,b,c);
+    system("pause");
+}
---
+++ 第二章 基本数据类型与常用库函数/ex10-3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    printf("%o %d %x",a,a,a);
+    system("pause");
+}
